73b11e25e9b67e22f78fb55d9de8d8a3d557552c
Evgeniy <tverdune@ya.ru>
2021-02-10 15:38:36 +0300

    089 Препроцессор JSX _ Практика

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,48 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import App from './components/app/';
 
-const Header = () => {
-  return <h2>Hello World!</h2>
-}
-
-const Field = () => {
-  const holder = 'Enter here';
-  const styleField = {
-    width: '310px'
-  }
-
-  return <input
-    style={styleField}
-    type='text'
-    placeholder={holder}
-    autoComplete=''
-    className='first'
-    htmlFor='' />
-}
-
-const Btn = () => {
-  const text = 'Log in';
-  const logged = true;
-
-  return <button> {logged ? 'Enter' : text} </button>
-}
-
-const App = () => {
-  return (
-    <div>
-      <Header/>
-      <Field/>
-      <Btn/>
-    </div>
-  )
-}
+import './index.css';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
